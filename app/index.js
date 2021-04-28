e9a800e2fea878cf067562aa340465c2be00f631
Sébastien Lampazona <sebastien@oclock.io>
2021-04-28 14:51:41 +0200

    Méthode create d'un tag

--- app/index.js
+++ app/index.js
@@ -4,6 +4,26 @@ dotenv.config();
 const Tag = require('./models/Tag');
 
 // Solution model
-Tag.findAll((err, tags) => {
-  console.log("findAll Callback", {err, tags})
+// Tag.findAll((err, tags) => {
+//   console.log("findAll Callback", {err, tags})
+// });
+
+// Tag.findOne(2, (err, tag) => {
+//   console.log("findOne Callback", {err, tag})
+// });
+
+// J'ai besoin de créer un tag, je créé donc une nouvelle instance
+// de mon tag, et je lui donne un nom
+// pas d'id puisque il existe pas encore en BDD, on va laisser
+// pg gérer l'auto incrémentation de l'id, c'est lui tout seul
+// qui va me remplir l'id dans mon instance
+const tagToCreate = new Tag({
+  name: 'Mon nouveau Tag',
+});
+
+// Instance crée, j'ai donc maintenant besoin de l'insérer en BDD
+// Je déclenche une fonction qui permet de le faire
+tagToCreate.create((err, createdTag) => {
+  // Ici l'insertion est terminée
+  // createdTag est littéralement tagToCreate
 });
--- app/models/CoreModel.js
+++ app/models/CoreModel.js
@@ -1,16 +1,27 @@
 class CoreModel {
-  _id;
+  _id = null;
 
-  constructor(obj){
+  constructor(obj) {
     this.id = obj.id;
   }
 
   // Setter / Getter de id
   set id(id) {
-    if (isNaN(parseInt(id, 10))){
-      throw new Error('Id must be an integer');
+    // Si l'id vaut quelque chose
+    if (id) {
+      // Alors on vérifie son intégrité
+      if (isNaN(parseInt(id, 10))) {
+        // Si c'est pas bon on fait planter
+        throw new Error('Id must be an integer');
+      }
+      // Si c'est bon le set
+      this._id = parseInt(id, 10);
     }
-    this._id = parseInt(id, 10);
+    // Si jamais l'id ne vaut rien
+    else {
+      // On impose l'id à null
+      this._id = null;
+    }
   }
 
   get id() {
