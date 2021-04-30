ab36d0f93e1b903ffe6fbbdd617ecabf545b850d
Sébastien Lampazona <sebastien@oclock.io>
2021-04-30 13:15:55 +0200

    Ajout de la récupération du tag qui a pour id 2

--- app/index.js
+++ app/index.js
@@ -3,6 +3,7 @@ dotenv.config();
 const sequelize = require('./database');
 
 const Question = require('./models/Question');
+const Tag = require('./models/Tag');
 
 // Récupération de la liste de toutes les questions
 Question.findAll()
@@ -20,3 +21,17 @@ Question.findAll()
 })
 
 // Récupération du Tag qui a pour id 2 --> Go RTFM
+// cf https://sequelize.org/master/manual/model-querying-finders.html
+Tag.findByPk(2)
+// Si ça s'est bien passé, j'ai donc mon Tag qui a pour id 2
+.then((tag) => {
+  console.log("Mon tag recherché", tag);
+})
+// Si ça s'est pas bien passé pour n'importe quelle raison
+// alors je passe dans le catch
+.catch((error) => {
+  console.error("Erreur capturée", {
+    name: error.name, 
+    message: error.message
+  });
+});
