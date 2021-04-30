685144c2cd019cda80a33280d7e5bd395961c27f
Sébastien Lampazona <sebastien@oclock.io>
2021-04-30 11:35:54 +0200

    Refacto model Question

--- app/index.js
+++ app/index.js
@@ -4,20 +4,3 @@ const sequelize = require('./database');
 
 const Level = require('./models/Level');
 
-// On test la connexion à la BDD
-sequelize.authenticate()
-  // Si c'est connecté on passe dans le then
-  .then(() => {
-    console.log('Connection has been established successfully.');
-
-    Level.findAll()
-      .then((level) => {
-        console.log("Level :", level)
-      });
-  })
-  // Si y'a eu une erreur quelle qu'elle soit, on passe dans le catch
-  .catch((error) => {
-    console.error('Unable to connect to the database:', error);
-  });
-
-
--- app/models/Question.js
+++ app/models/Question.js
@@ -1,69 +1,27 @@
-const CoreModel = require('./CoreModel')
+const { DataTypes, Model } = require('sequelize');
+const sequelize = require('../database');
 
-class Question extends CoreModel{
-  
-  static tableName = "question";
+class Question extends Model {}
 
-  _question;
-  _anecdote;
-  _wiki;
-  _level_id;
-  _answer_id;
-  _quiz_id;
+Question.init({
+  question: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  anecdote: {
+    type: DataTypes.STRING,
+  },
+  wiki: {
+    type: DataTypes.STRING,
+  },
+}, {
+  // Autre options du modeles
+  sequelize, // On a besoin de donner l'instance de la connexion
+  timestamps: false, // Pour ne pas avoir les champs createdAt et updatedAt
+  tableName: 'question' // Pour imposer un nom de table, sinon il prend le model au pluriel par défaut
+});
 
-  constructor(obj){
-    super(obj);
-    this.question = obj.question;
-    this.anecdote = obj.anecdote;
-    this.wiki = obj.wiki;
-    this.level_id = obj.level_id;
-    this.answer_id = obj.answer_id;
-    this.quiz_id = obj.quiz_id;
-  }
-
-
-  get question() {
-    return this._question;
-  }
-
-  set question(question){
-    this._question = question;
-  }
-  get anecdote() {
-    return this._anecdote;
-  }
-
-  set anecdote(anecdote){
-    this._anecdote = anecdote;
-  }
-  get wiki() {
-    return this._wiki;
-  }
-
-  set wiki(wiki){
-    this._wiki = wiki;
-  }
-  get level_id() {
-    return this._level_id;
-  }
-
-  set level_id(level_id){
-    this._level_id = level_id;
-  }
-  get answer_id() {
-    return this._answer_id;
-  }
-
-  set answer_id(answer_id){
-    this._answer_id = answer_id;
-  }
-  get quiz_id() {
-    return this._quiz_id;
-  }
-
-  set quiz_id(quiz_id){
-    this._quiz_id = quiz_id;
-  }
-}
+// Puis on export pour pouvoir le require là ou on va l'instancier
+module.exports = Question;
 
 module.exports = Question;
