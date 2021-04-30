ba4d3be6cedaa0badd3df3a3a8646ba0fe1c71e3
Sébastien Lampazona <sebastien@oclock.io>
2021-04-30 11:39:58 +0200

    Refacto model User

--- app/models/User.js
+++ app/models/User.js
@@ -1,63 +1,29 @@
-const CoreModel = require('./CoreModel');
-const database = require('../database');
+const { DataTypes, Model } = require('sequelize');
+const sequelize = require('../database');
 
-class User extends CoreModel {
+class User extends Model {}
 
-  static tableName = 'user';
+User.init({
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  firstname: {
+    type: DataTypes.STRING,
+  },
+  lastname: {
+    type: DataTypes.STRING,
+  },
+}, {
+  // Autre options du modeles
+  sequelize, // On a besoin de donner l'instance de la connexion
+  timestamps: false, // Pour ne pas avoir les champs createdAt et updatedAt
+  tableName: 'user' // Pour imposer un nom de table, sinon il prend le model au pluriel par défaut
+});
 
-  _email;
-  _password;
-  _firstname;
-  _lastname;
-
-  constructor(obj) {
-    super(obj)
-    this.email = obj.email;
-    this.password = obj.password;
-    this.firstname = obj.firstname;
-    this.lastname = obj.lastname;
-  }
-
-  // Set / Get de email
-  set email(email) {
-    this._email = email;
-  }
-
-  get email() {
-    return this._email;
-  }
-
-  // Set / Get de password
-  set password(password) {
-    this._password = password;
-  }
-
-  get password() {
-    return this._password;
-  }
-
-  // Set / Get de firstname
-  set firstname(firstname) {
-    this._firstname = firstname;
-  }
-
-  get firstname() {
-    return this._firstname;
-  }
-
-  // Set / Get de lastname
-  set lastname(lastname) {
-    this._lastname = lastname;
-  }
-
-  get lastname() {
-    return this._lastname.toUpperCase();
-  }
-
-  get fullName() {
-    return this.firstname + ' ' + this.lastname
-  }
-
-}
-
+// Puis on export pour pouvoir le require là ou on va l'instancier
 module.exports = User;
